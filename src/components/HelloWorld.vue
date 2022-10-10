aa8cd1cf52ab6285723eb24dfaff3d905cf2efff
farnazed <farnaz.e.daneshvar@gmail.com>
2022-10-10 16:49:49 -0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+  <div class="emojiwrapper">
+    <p class="drop">
+      <span class="pumpkin">{{ randomEmoji }}</span>
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  computed: {
+    randomEmoji() {
+      return String.fromCodePoint(
+        '0x' +
+          ['1F47B', '1F383', '1f9df', '1F479'][Math.floor(Math.random() * 4)]
+      );
+    },
+  },
+};
+</script>
+<style scoped>
+.emojiwrapper {
+  display: flex;
+}
+
+.pumpkin {
+  font-size: 4rem;
+}
+
+.drop:hover {
+  animation: wiggle ease-in 0.5s;
+  animation-fill-mode: none;
+}
+
+.drop {
+  animation: drop ease-in 0.5s;
+  animation-fill-mode: both;
+}
+@keyframes drop {
+  0% {
+    transform: translatey(-100px);
+  }
+  60% {
+    transform: translatey(0px) scale(1.2, 1);
+  }
+  70% {
+    transform: translatey(-10px) scale(1, 1);
+  }
+  100% {
+    transform: translatey(0px) rotatez(0deg);
   }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+@keyframes wiggle {
+  0% {
+    transform: rotatez(0deg);
+  }
+  20% {
+    transform: rotatez(30deg);
+  }
+  40% {
+    transform: rotatez(-30deg);
+  }
+  60% {
+    transform: rotatez(20deg);
+  }
+  80% {
+    transform: rotatez(-20deg);
+  }
+  100% {
+    transform: rotatez(0deg);
+  }
 }
 </style>
